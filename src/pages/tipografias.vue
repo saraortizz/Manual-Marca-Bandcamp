b5f09288eb49ddd11487c01f2d8092c3cdd9e39d
saraortizz <saraortizzcardo25@gmail.com>
2026-05-17 17:42:39 +0200

    Arreglado tipografias.vue

--- src/pages/tipografias.vue
+++ src/pages/tipografias.vue
@@ -1,5 +1,4 @@
 <script setup>
-<<<<<<< HEAD:src/tipografias.vue
 import Header from '@/components/Header.vue'
 import Hero from '@/components/Hero.vue'
 import heroTipografias from 'src/assets/Images/Tipografia/Hero - Tipografía.jpg'
@@ -11,9 +10,6 @@ import logoHuellaColores from '@/assets/images/logo/logo-huella-colores.png'
 import areaRespeto from '@/assets/images/logo/area-de-respeto.png'
 import convivenciaMarcas from '@/assets/images/logo/logo-con-mas-marcas.png'
 import separador from '@/assets/images/Linea1.png'
-=======
-import heroTipografias from '@/assets/images/tipografia/hero-tipografia.jpg'
->>>>>>> 7b94fbe5c635cc274c1099ef9d66c2b47784130b:src/pages/tipografias.vue
 </script>
 
 <template>
