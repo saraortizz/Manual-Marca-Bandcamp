9a62af900bbf59e00f42d35f971eb0a07322f5f5
Carmen Valverde <carmenvalverde@MacBook-Pro.local>
2026-05-17 16:53:43 +0200

    logocosas

--- src/tipografias.vue
+++ src/tipografias.vue
@@ -2,6 +2,14 @@
 import Header from '@/components/Header.vue'
 import Hero from '@/components/Hero.vue'
 import heroTipografias from 'src/assets/Images/Tipografia/Hero - Tipografía.jpg'
+import logoPrincipal from '@/assets/images/logo/bandcamp-logo.png'
+import colores from '@/assets/images/logo/bandcamp-logo-colores-principales.png'
+import imgexcepcional from '@/assets/images/logo/logo-con-huella-prueba.png'
+import logoHuella from '@/assets/images/logo-huella.png'
+import logoHuellaColores from '@/assets/images/logo/logo-huella-colores.png'
+import areaRespeto from '@/assets/images/logo/area-de-respeto.png'
+import convivenciaMarcas from '@/assets/images/logo/logo-con-mas-marcas.png'
+import separador from '@/assets/images/Linea1.png'
 </script>
 
 <template>
